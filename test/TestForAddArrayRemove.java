cfe4d3df3d2eb2f3a5bc2be6624714b02fd7ff2e
leptodon <leptodon@users.noreply.github.com>
2019-04-09 21:41:42 +0200

    main

--- test/TestForAddArrayRemove.java
+++ test/TestForAddArrayRemove.java
@@ -1,14 +1,15 @@
-package com.javarush.task.test;
+package com.javarush.test;
 
 import java.util.Arrays;
 import java.util.Collections;
 
 public class TestForAddArrayRemove {
     public static void main(String[] args) {
-        long a = System.currentTimeMillis();
+/*        long a = System.currentTimeMillis();
         for (long i = 0; i < Long.MAX_VALUE; i++) {
         }
         long b = System.currentTimeMillis();
-        System.out.println((b-a)+"ms");
-    }
+        System.out.println((b-a)+"ms");*/
+
+ }
 }
